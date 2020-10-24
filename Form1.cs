501e270a030b08172b16e977d08334759f1c627b
Jasdeep Brar <uhitsjasdeep@gmail.com>
2020-10-23 22:41:13 -0700

    Added new code for User object

--- Form1.cs
+++ Form1.cs
@@ -1,9 +1,11 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -12,9 +14,45 @@ namespace Blue_Ward
 {
     public partial class Form1 : Form
     {
+        private string summonerName;
+        User[] userList = new User[5];
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            summonerName = summonerNameTxtBox.Text;
+
+            for (int i = 0; i < userList.Length; i++)
+            {
+                if (userList[i] == null)
+                {
+                    userList[i] = new User(summonerName);
+                    PopulateUser(i);    //turn into a gui output
+                    return;
+                }
+
+                if (i == userList.Length && userList[i] != null)
+                {
+                    Console.WriteLine("Out of summoner space");
+                }
+            }
+        }
+
+        public async void PopulateUser(int userID)
+        {
+            HttpClient client = new HttpClient();
+            HttpResponseMessage response = await client.GetAsync("https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/" + summonerName + "?api_key=RGAPI-b5585198-babd-491e-80ac-6cf78ae20ea2");
+
+            if (response != null)
+            {
+                string jsonString = await response.Content.ReadAsStringAsync();
+
+                userList[userID] = JsonConvert.DeserializeObject<User>(jsonString);
+            }
         }
     }
 }
--- User.cs
+++ User.cs
@@ -1,16 +1,38 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace Blue_Ward
 {
-    private static readonly
-    class User
+    public class User
     {
-        
+        public string id {get; set;}
+        public string accountId { get; set; }
+        public string puuid { get; set; }
+        public string name { get; set; }
+        public string profileIconId { get; set; }
+        public int summonerLevel { get; set; }   
 
+        public User(string username)
+        {
+            name = username;
+        }
+
+        public void print()
+        {
+            Console.WriteLine("Name: " + name);
+            Console.WriteLine("id: " + id + "\n");
+            Console.WriteLine("accountId: " + accountId + "\n");
+            Console.WriteLine("puuid: " + puuid + "\n");
+            Console.WriteLine("profileIconId: " + profileIconId + "\n");
+            Console.WriteLine("summonerLevel: " + summonerLevel + "\n");
+
+        }
     }
 }
