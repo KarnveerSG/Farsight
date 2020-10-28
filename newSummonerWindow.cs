81222819f5650ab796277a5898ed30bca9b41fe5
Jasdeep Brar <uhitsjasdeep@gmail.com>
2020-10-27 18:59:36 -0700

    Added Data Dragon JSON files to project and deserialized champions keys into a data structure.

--- newSummonerWindow.cs
+++ newSummonerWindow.cs
@@ -38,7 +38,7 @@ namespace Blue_Ward
         public async Task PopulateUser()
         {
             HttpClient client = new HttpClient();
-            HttpResponseMessage response = await client.GetAsync("https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/" + summonerName + "?api_key=RGAPI-47b3da84-5c75-4204-87b4-746494dd4453");
+            HttpResponseMessage response = await client.GetAsync("https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/" + summonerName + "?api_key=RGAPI-a8d0fd7f-2073-4232-867b-ed5d4912e651");
 
             if (response != null)
             {
